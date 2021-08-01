3df8bd1ac7395b5c9c378ade899156715d24d262
masoumehaemmi <80619179+masoumehaemmi@users.noreply.github.com>
2021-08-01 21:44:49 +0430

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,17 @@
+import math
+x = 0
+while  True :
+
+    num = input(" enter your number for end enter exit : ")
+
+    if num == "exit" :
+      break
+    
+    else :
+        
+         
+         x = int(num) + x 
+
+print (x)    
+
+ 
